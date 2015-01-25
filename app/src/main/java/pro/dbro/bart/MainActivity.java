557ede978d2125f7c9eac12b6b6f5bb1863dabd2
David Brodsky <davidpbrodsky@gmail.com>
2015-01-25 14:16:21 -0800

    mundane detail
    service wasn’t being properly unbound

--- app/src/main/java/pro/dbro/bart/MainActivity.java
+++ app/src/main/java/pro/dbro/bart/MainActivity.java
@@ -124,7 +124,7 @@ public class MainActivity extends Activity implements BartApiDelegate, ServiceCo
     public void onStop() {
         super.onStop();
         saveInput();
-        if (!mServiceBound) {
+        if (mServiceBound) {
             unBindService();
         }
     }
